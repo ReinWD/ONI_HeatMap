35d6854df40fe10b267d70664985ec08d87e96d5
ReinWD <ReinWDD@gmail.com>
2021-07-29 20:23:19 +0800

    basic function

--- Patches.cs
+++ Patches.cs
@@ -4,29 +4,41 @@ namespace HeatMapEnhanced
 {
 	public class Patches
 	{
-		[HarmonyPatch(typeof(SimDebugView))]
-		[HarmonyPatch("TemperatureToColor")]
+		[HarmonyPatch(typeof(OverlayModes.Temperature))]
+		[HarmonyPatch("Enable")]
 		public class TemperatureToColorPatch
 		{
 			static bool effect = false;
+			static SimDebugView.ColorThreshold[] colorThreshold;
+			
 			public static void Prefix()
 			{
 				if(!effect){
-					float lowerBound = 273;
-					SimDebugView.ColorThreshold[] colorThreshold = SimDebugView.Instance.temperatureThresholds;
-					string a = "colorThreshold: "+ colorThreshold;
-					Debug.Log(a);
-					
-					for(int i = 0; i < colorThreshold.Length; i++){
-
+					SimDebugView.ColorThreshold[] tmp = SimDebugView.Instance.temperatureThresholds;
+					colorThreshold = new SimDebugView.ColorThreshold[tmp.Length];
+					for (int i = 0; i<tmp.Length ; i++){
+						colorThreshold[i] = tmp[i];
 					}
+					effect = true;
 				}
-				Debug.Log("I execute before SimDebugView.TemperatureToColor!");
+				float lowerBound = 273 + 125;
+				string a = "modified: ";
+				string aa = "original: ";
+				string aaa = "factor: ";
+				float maxTemp = 273 + 500;
+				float step = (maxTemp - lowerBound) / (colorThreshold.Length - 2);
+					for(int i = 1; i < colorThreshold.Length - 1; i++){
+						float currentVal = colorThreshold[i].value;					
+						float b = lowerBound + step * (i-1);
+						SimDebugView.Instance.temperatureThresholds[i].value = b;
+						Debug.Log(a+b.ToString());
+						Debug.Log(aa+colorThreshold[i].value.ToString());
+						Debug.Log(aaa+(currentVal / maxTemp).ToString());
+					}
 			}
 
 			public static void Postfix()
 			{
-				Debug.Log("I execute after SimDebugView.TemperatureToColor!");
 			}
 		}
 		
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("$safeprojectname$")]
+[assembly: AssemblyTitle("HeatMap")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("$safeprojectname$")]
+[assembly: AssemblyProduct("HeatMap")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
