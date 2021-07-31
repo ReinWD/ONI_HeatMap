f6b4b8a41fc9f18f20805c6c44a2660807a1d074
ReinWD <ReinWDD@gmail.com>
2021-07-31 14:54:37 +0800

    clean up

--- src/HeatMapPatch.cs
+++ src/HeatMapPatch.cs
@@ -2,15 +2,17 @@ using HarmonyLib;
 
 namespace HeatMapEnhanced
 {
-	public class Patches
+	public class HeatMap
 	{
 		[HarmonyPatch(typeof(OverlayModes.Temperature))]
 		[HarmonyPatch("Enable")]
-		public class TemperatureToColorPatch
-		{
+		public class OnEnable{
 			static bool effect = false;
 			static SimDebugView.ColorThreshold[] colorThreshold;
-			
+			static float lowerBound = 273 + 125;
+			static float maxTemp = 273 + 500;
+			static bool modified = false;
+
 			public static void Prefix()
 			{
 				if(!effect){
@@ -19,29 +21,45 @@ namespace HeatMapEnhanced
 					for (int i = 0; i<tmp.Length ; i++){
 						colorThreshold[i] = tmp[i];
 					}
+					lowerBound = tmp[1].value;
+					lowerBound = tmp[tmp.Length-2].value;
 					effect = true;
 				}
-				float lowerBound = 273 + 125;
 				string a = "modified: ";
 				string aa = "original: ";
 				string aaa = "factor: ";
-				float maxTemp = 273 + 500;
 				float step = (maxTemp - lowerBound) / (colorThreshold.Length - 2);
-					for(int i = 1; i < colorThreshold.Length - 1; i++){
+
+				HeatMap.showUI();
+				for(int i = 1; i < colorThreshold.Length - 1; i++){
+					if(modified){
 						float currentVal = colorThreshold[i].value;					
 						float b = lowerBound + step * (i-1);
 						SimDebugView.Instance.temperatureThresholds[i].value = b;
 						Debug.Log(a+b.ToString());
 						Debug.Log(aa+colorThreshold[i].value.ToString());
 						Debug.Log(aaa+(currentVal / maxTemp).ToString());
+					}else{
+						SimDebugView.Instance.temperatureThresholds[i] = tmp[i]
 					}
+				}
 			}
 
-			public static void Postfix()
+		
+		}
+
+		public static void showUI(){}
+
+		public static void hideUI(){}
+		
+		[HarmonyPatch(typeof(OverlayModes.Temperature))]
+		[HarmonyPatch("Disable")]
+		public class OnDisable{
+			public static void Prefix()
 			{
+				HeatMap.hideUI();
 			}
 		}
-		
 		
 	}
 }
